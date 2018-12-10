8e0b840fc32212b21845c98deadd3173d0194a96
Nora0713 <1379026619@qq.com>
2018-12-10 22:48:45 +0800

    :banana:

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,10 +1,11 @@
 import {Game} from 'phaser';
 
+import {BoardScene} from './scenes/board-scene';
 import {LoginScene} from './scenes/login-scene';
 import {gameHeight, gameWidth} from './utils/ratio';
 
 let config: GameConfig = {
-  type: Phaser.AUTO,
+  type: Phaser.CANVAS,
   width: gameWidth,
   height: gameHeight,
   physics: {
@@ -18,7 +19,8 @@ let config: GameConfig = {
 let game = new Game(config);
 
 game.scene.add('LoginScene', new LoginScene());
+game.scene.add('BoardScene', new BoardScene());
 
-game.scene.start('LoginScene');
+game.scene.start('BoardScene');
 
 export {game};
---
+++ client/src/scenes/board-scene.ts
@@ -0,0 +1,44 @@
+import {Game, Scene} from 'phaser';
+
+import {gameHeight, gameWidth, ratio} from '../utils/ratio';
+
+export class BoardScene extends Scene {
+  private gameOptions = {
+    landWidth: 183 * ratio,
+    landHeight: 109 * ratio,
+    landCount: 5,
+    padding: 120 * ratio,
+  };
+
+  private landGroup!: Phaser.GameObjects.Group;
+
+  constructor() {
+    super({key: 'BoardScene'});
+  }
+
+  preload(): void {
+    // this.load.setBaseURL('http://labs.phaser.io');
+    this.load.image('green-block', 'assets/green-block.png');
+  }
+
+  create(): void {
+    this.landGroup = this.add.group();
+    this.add.image(800, 600, 'green-block');
+  }
+
+  addLandX(): void {
+    let offset = {x: 0, y: 0};
+    offset.x = gameWidth / 2;
+    offset.y = gameHeight - this.gameOptions.padding;
+    console.log(offset.y);
+
+    for (let j = 0; j < this.gameOptions.landCount; j++) {
+      let landX = this.gameOptions.landWidth * j + offset.x;
+      let landY = offset.y - ((this.gameOptions.landHeight * j) / 4) * 3;
+      let land = this.add.sprite(landX, landY, 'green-block');
+      console.log(landX, landY);
+      land.setOrigin(0, 0);
+      this.landGroup.add(land);
+    }
+  }
+}
