59b623eebac36866f3bcbf4ac1c1cc1fceef5035
Dizy Zeng <mhisf@vip.qq.com>
2018-12-10 20:45:32 +0800

    Add public to static server

--- server/src/paths.ts
+++ server/src/paths.ts
@@ -7,3 +7,4 @@ export const SERVER_DIR = Path.join(PROJECT_DIR, 'server');
 export const CLIENT_DIR = Path.join(PROJECT_DIR, 'client');
 
 export const CLIENT_BUILD_DIR = Path.join(CLIENT_DIR, 'bld');
+export const CLIENT_PUBLIC_DIR = Path.join(CLIENT_DIR, 'public');
--- server/src/services/http-service.ts
+++ server/src/services/http-service.ts
@@ -2,7 +2,7 @@ import {Server} from 'http';
 
 import express from 'express';
 
-import {CLIENT_BUILD_DIR} from '../paths';
+import {CLIENT_BUILD_DIR, CLIENT_PUBLIC_DIR} from '../paths';
 
 export class HTTPService {
   constructor(private app: express.Express, private httpServer: Server) {
@@ -15,5 +15,6 @@ export class HTTPService {
 
   private initialize(): void {
     this.app.use(express.static(CLIENT_BUILD_DIR));
+    this.app.use(express.static(CLIENT_PUBLIC_DIR));
   }
 }
