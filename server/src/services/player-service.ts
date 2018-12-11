dffa629dfec016f6e8e05a4404c175448029cb32
Dizy Zeng <mhisf@vip.qq.com>
2018-12-12 01:47:20 +0800

    Try event driven join

--- server/src/services/player-service.ts
+++ server/src/services/player-service.ts
@@ -1,4 +1,4 @@
-import {ModelService, Player} from 'shared';
+import {ModelService, Player, packModel} from 'shared';
 
 import {SocketService} from './socket-service';
 
@@ -6,7 +6,9 @@ export class PlayerService {
   constructor(
     private socketService: SocketService,
     private modelService: ModelService,
-  ) {}
+  ) {
+    this.initialize();
+  }
 
   private initialize(): void {
     this.socketService.io.on('connect', socket => {
@@ -16,7 +18,16 @@ export class PlayerService {
 
   private initializeSocket(socket: SocketIO.Socket): void {
     socket.on('player:join', (name: string) => {
+      if (this.modelService.hasModel('player', name)) {
+        socket.emit('player:fail', 'player:join', 201, '玩家名字已经存在啦！');
+        return;
+      }
+
       let player = new Player(name);
+
+      this.modelService.addModel('player', player);
+
+      socket.emit('play:success', 'player:join', packModel(player));
     });
   }
 }
