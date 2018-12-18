ca62e4991890690d3a0dadc30a4533a8f7d9f496
Dizy Zeng <mhisf@vip.qq.com>
2018-12-18 23:41:00 +0800

    Add more tests

--- server/test/basic.test.ts
+++ server/test/basic.test.ts
@@ -3,6 +3,8 @@
 import SocketIO from 'socket.io-client';
 import {runServer, stopServer} from './utils';
 
+const PLAYER_NAME = 'Dizy';
+
 jest.setTimeout(999999);
 
 let io = SocketIO('http://localhost:8090');
@@ -21,15 +23,60 @@ test('player:login', () => {
       resolve({action, playerTransfer});
     });
 
-    io.on('player:fail', (action: string, playerTransfer: object) => {
-      resolve({action, playerTransfer});
+    io.on('player:fail', (action: string, code: number) => {
+      resolve({action, code});
     });
 
-    io.emit('player:login', 'Dizy');
+    io.emit('player:login', PLAYER_NAME);
   });
 
   return promise.then(({action, playerTransfer}) => {
     expect(action).toBe('player:login');
-    expect(playerTransfer).toHaveProperty('id', 'Dizy');
+    expect(playerTransfer).toHaveProperty('id', PLAYER_NAME);
+  });
+});
+
+let roomId: string = '';
+
+test('create room', () => {
+  let promise = new Promise<any>(resolve => {
+    io.on('room:success', (action: string, roomTransfer: object) => {
+      resolve({action, roomTransfer});
+    });
+
+    io.on('room:fail', (action: string, code: number) => {
+      resolve({action, code});
+    });
+
+    io.emit('room:create');
+  });
+
+  return promise.then(({action, roomTransfer}) => {
+    expect(action).toBe('room:create');
+    expect(roomTransfer).toHaveProperty('id');
+
+    if ('id' in roomTransfer) {
+      roomId = roomTransfer['id'];
+    }
+  });
+});
+
+test('join room', () => {
+  let promise = new Promise<any>(resolve => {
+    io.on('room:success', (action: string, roomTransfer: object) => {
+      resolve({action, roomTransfer});
+    });
+
+    io.on('room:fail', (action: string, code: number) => {
+      resolve({action, code});
+    });
+
+    io.emit('room:join', roomId);
+  });
+
+  return promise.then(({action, roomTransfer}) => {
+    expect(action).toBe('room:join');
+    expect(roomTransfer).toHaveProperty('id', roomId);
+    expect(roomTransfer['data']['players']).toContain(PLAYER_NAME);
   });
 });
